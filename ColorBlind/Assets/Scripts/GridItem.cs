f283d8e3118d6a39032ee5642ac44ed8edadbfa7
Anuj Kaura <akaura@kiwiup.com>
2015-01-12 12:50:49 +0530

    Manager and Grid Item

---
+++ ColorBlind/Assets/Scripts/GridItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GridItem : MonoBehaviour {
+
+	Manager manager;
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+	}
+
+	public void ButtonClick(){
+		if(manager.timer <=0)
+			return;
+		Image image = this.gameObject.GetComponent <Image>();
+
+		if(!image.color.Equals(manager.color)){
+			manager.LevelUp();
+		}
+	}
+}
---
+++ ColorBlind/Assets/Scripts/Manager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+	public Text Score;
+	public Text Timer;
+	[HideInInspector]
+	public int level = 2;
+	[HideInInspector]
+	public Color color = Color.red;
+	[HideInInspector]
+	public int score = 0;
+	[HideInInspector]
+	public int timer = 30;
+
+	private float startTime, currentTime;
+
+	void Start () {
+		startTime = Time.time;
+		Score.text = ""+score;
+		Timer.text = ""+timer;
+	}
+
+	void Update () {
+		currentTime = Time.time - startTime;
+		timer = Mathf.CeilToInt(timer - currentTime);
+		if(timer <=0){
+			//End Game
+		} 
+		else
+			Timer.text = ""+timer;
+
+	}
+
+	void RecreateGrid(int level){
+		//Recreate level x level grid
+		//Change color
+	}
+
+	public void LevelUp(){
+		level++;
+		score++;
+		timer = 30;
+		Score.text = ""+score;
+		Timer.text = ""+timer;
+		RecreateGrid(level);
+	}
+
+}
