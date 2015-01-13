561506f174d8c6a749f9eb1a2c98da6c1afc3f76
Anshul <ajhawar@kiwiup.com>
2015-01-13 15:23:00 +0530

    hint for grid item

--- ColorBlind/Assets/Scripts/GridItem.cs
+++ ColorBlind/Assets/Scripts/GridItem.cs
@@ -5,17 +5,51 @@ using UnityEngine.UI;
 public class GridItem : MonoBehaviour {
 
 	Manager manager;
+	Image image;
+
+	public int MAX_HINT_TIMER = 5;
+	private float timer = 0;
+
+	private bool showHint = false;
+	private float hintDuration = 4;
+	private float lerpTime = 0;
+
 	// Use this for initialization
 	void Start () {
 		manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<Manager>();
+		image = this.gameObject.GetComponent <Image>();
+		timer = MAX_HINT_TIMER;
+	}
+
+	void Update(){
+		if(!image.color.Equals(manager.levelColor)){
+			timer -= Time.deltaTime;
+			if(timer <=0){
+				this.showHint = true;
+			} 
+		}
+
+		if (showHint) {
+			this.ShowHint();
+		}
 	}
 
 	public void ButtonClick(){
-
-		Image image = this.gameObject.GetComponent <Image>();
-
 		if(!image.color.Equals(manager.levelColor)){
 			manager.LevelUp();
+		}
+	}
+
+	private void ShowHint(){
+//		Debug.LogError ("CAme herererer");
+//		Color color = image.color;
+//		color.r = color.r + 100/255;
+//		image.color = manager.levelColor;
+
+		image.color = Color.Lerp (image.color, manager.levelColor, lerpTime);
+
+		if (lerpTime < 1) {
+			lerpTime += Time.deltaTime/hintDuration;
 		}
 	}
 }
