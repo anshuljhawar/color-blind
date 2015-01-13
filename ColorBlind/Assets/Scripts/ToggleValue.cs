ad44f2ec72938744fb100af859835a6aadc720bf
Anuj Kaura <akaura@kiwiup.com>
2015-01-13 13:24:29 +0530

    Added Difficulty Level

---
+++ ColorBlind/Assets/Scripts/ToggleValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// This is just an example, normaly the values would be something meaningful for the application
+/// </summary>
+
+public class ToggleValue : MonoBehaviour
+{
+    public enum ToggleValues { Easy, Medium, Hard }
+
+    [SerializeField]
+    private ToggleValues value = ToggleValues.Easy;
+    public ToggleValues Value { get { return value; } set { this.value = value; } }
+
+    private void Start()
+    {
+        GetComponentInChildren<Text>().text = value.ToString();
+    }
+}
