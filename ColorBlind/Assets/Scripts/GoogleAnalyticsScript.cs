da39cc7ae59054bdd66c07b5faeb2f9202e6baac
Anshul <ajhawar@kiwiup.com>
2015-01-16 11:32:36 +0530

    google analytics

---
+++ ColorBlind/Assets/Scripts/GoogleAnalyticsScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoogleAnalyticsScript : MonoBehaviour {
+
+	public GoogleAnalyticsV3 googleAnalytics;
+
+	// Use this for initialization
+	void Start () {
+		// Reports that the user is viewing the Main Menu
+		if (googleAnalytics)
+			googleAnalytics.LogScreen("Main Menu");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
